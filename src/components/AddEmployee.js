2aa5fd95bb6fc112f7cec7ec2f5d5b641720c724
sao333167 <sao333167@gmail.com>
2023-10-11 13:35:58 +0700

    Add modal add employee

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee(props) {
-  const [name, setName] = useState(props.name);
-  const [role, setRole] = useState(props.role);
+function AddEmployee(props) {
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('');
 
     
     
@@ -17,9 +17,9 @@ function EditEmployee(props) {
     <>
       <button
         onClick={handleShow}
-        className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+        className="block mx-auto px-4 py-2 mt-10 text-sm bg-purple-400 text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
       >
-        Update
+        + Add Employee
       </button>
       {/* <Button variant="primary" onClick={handleShow}>
         Launch static backdrop modal
@@ -32,14 +32,18 @@ function EditEmployee(props) {
         keyboard={false}
       >
         <Modal.Header closeButton className="text-gray-500">
-          <Modal.Title>Update Employee</Modal.Title>
+          <Modal.Title>+ Add Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-                  <form onSubmit={(e) => {
-                      handleClose();
-                      e.preventDefault();
-                      props.updateEmployee(props.id, name, role);
-          }} id="editmodal" className="w-full max-w-sm">
+          <form
+            onSubmit={(e) => {
+              handleClose();
+              e.preventDefault();
+              props.updateEmployee(props.id, name, role);
+            }}
+            id="editmodal"
+            className="w-full max-w-sm"
+          >
             <div className="md:flex md:items-center mb-6">
               <div className="md:w-1/3">
                 <label
@@ -51,8 +55,9 @@ function EditEmployee(props) {
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
+                  placeholder="Name"
                   type="text"
                   value={name}
                   onChange={(e) => {
@@ -72,8 +77,32 @@ function EditEmployee(props) {
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="role"
+                  placeholder="Role"
+                  type="text"
+                  value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="img"
+                >
+                  Image URL
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="img"
+                  placeholder="https://google.com/"
                   type="text"
                   value={role}
                   onChange={(e) => {
@@ -91,11 +120,11 @@ function EditEmployee(props) {
           >
             Close
           </button>
-                  <button
+          <button
             form="editmodal"
             className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
           >
-            Update
+            Add
           </button>
         </Modal.Footer>
       </Modal>
@@ -103,4 +132,4 @@ function EditEmployee(props) {
   );
 }
 
-export default EditEmployee;
+export default AddEmployee;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,7 +3,7 @@ import EditEmployee from "./EditEmployee";
 export default function Employee(props) {
   return (
     <>
-      <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img
           className="object-cover rounded-full h-[100px] w-[100px] block mx-auto sm:mx-0 sm:shrink-0"
           src={props.img}
@@ -13,9 +13,13 @@ export default function Employee(props) {
           <div className="space-y-0.5">
             <p className="text-lg text-black font-semibold">{props.name}</p>
             <p className="text-slate-500 font-medium">{props.role}</p>
-                  </div>
-                  <EditEmployee />
-          
+          </div>
+          <EditEmployee
+            id={props.id}
+            name={props.name}
+            role={props.role}
+            updateEmployee={props.updateEmployee}
+          />
         </div>
       </div>
     </>
